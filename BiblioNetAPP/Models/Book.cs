ebc7fd2bd8077b5b702a6c96c60e00a1d350625c
LeinerViloria <vilorialeiner2002@gmail.com>
2022-03-17 11:43:03 -0500

    Se validan los libros

---
+++ BiblioNetAPP/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BiblioNetAPP.Models
+{
+    public class Book
+    {
+        public int Id { set; get; }
+        [Required(ErrorMessage="{0} is required")]
+        public string BookName { set; get; }
+        public string Author { set; get; }
+        public float Price { set; get; }
+    }
+}
